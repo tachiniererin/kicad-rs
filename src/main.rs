b77fe6631d94235ae11f43ebc18a18c102c0b9cd
Elen Eisendle <ln@calcifer.ee>
2020-09-25 20:20:14 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use lexpr::{self, parse::Options};
 use std::fs;
 
+use serde_lexpr::{from_str_custom, to_string};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct Version {
+  name: String,
+  age: u8,
+}
+
+
 fn main() {
     println!("reading test pcb...");
+
+    let tuple: (u8, String, u64);
 
     let contents =
         fs::read_to_string("ferret.kicad_pcb").expect("Something went wrong reading the file");
@@ -15,8 +27,29 @@ fn main() {
         &contents
     };
 
-    let options = Options::kicad();    
-    let results = lexpr::from_str_custom(&contents, options);
+    let results = lexpr::from_str_custom(&contents, Options::kicad()).unwrap();
 
-    println!("{}", results.unwrap())
+    // the pcb structure
+    let pcb = results.as_pair().unwrap();
+    let mut iter = pcb.1.list_iter().unwrap();
+/*
+    let version = iter.next().unwrap();
+    let host = iter.next().unwrap();
+    let page = iter.next().unwrap();
+    let layers = iter.next().unwrap();
+    let setup = iter.next().unwrap();
+*/
+    for value in iter {
+        let v = value.to_vec().unwrap();
+        let sym = v.first().unwrap();
+
+        if !sym.is_cons() {
+            match sym.to_string().as_str() {
+                "version" => println!("{:#?}", v[1])
+                
+            }
+        } else {
+            println!("{:#?}", v.to_vec());
+        }
+    }
 }
